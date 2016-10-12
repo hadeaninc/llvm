a344ebcb00bee6472b45ffe40f02e48f4687bd7f
Francis Russell <francis@hadean.com>
2016-10-12 10:36:11 +0100

    XOR CALL64m target with 0.

--- lib/Target/X86/X86.h
+++ lib/Target/X86/X86.h
@@ -86,6 +86,9 @@ FunctionPass *createX86ExpandPseudoPass();
 /// the upper portions of registers, and to save code size.
 FunctionPass *createX86FixupBWInsts();
 
+// @HADEAN@
+FunctionPass *createX86HadeanExpandJumps();
+
 void initializeFixupBWInstPassPass(PassRegistry &);
 } // End llvm namespace
 
