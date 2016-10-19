09187d7062f382d2cd5401c0855038dafc87990e
Francis Russell <francis@hadean.com>
2016-10-19 11:53:58 +0100

    Reserve register R11 for indirect jump addresses.
    Rationale:
    
    Can't we just say that call/return/jump clobbers the register it uses for
    indirect jumps?
    
    No. This is only useful before register allocation. Unfortunately
    various passes insert control flow after register allocation which we
    also want to rewrite. We place our rewrites at the pre-emit state. At
    this point, code properties required by the register allocator no longer
    exist so it's not possible to use virtual registers and have the
    allocator patch it up.
    
    OK, let's make the inserted code preserve everything.
    
    For a jump this isn't possible since we have no way to restore the
    original register value.
    
    We *can* do this for calls, but it involves pushing the register to the
    stack before the call and restoring it after the call is complete. This
    should be safe, but it alters the stack layout and it would be best to
    be as transparent as possible.

--- lib/Target/X86/X86RegisterInfo.cpp
+++ lib/Target/X86/X86RegisterInfo.cpp
@@ -503,6 +503,16 @@ BitVector X86RegisterInfo::getReservedRegs(const MachineFunction &MF) const {
     }
   }
 
+  // @HADEAN@
+  // We reserve R11 for the address of indirect jumps
+  const X86Subtarget &subtarget = MF.getSubtarget<X86Subtarget>();
+  if (subtarget.isTargetHadean()) {
+    Reserved.set(X86::R11);
+    Reserved.set(X86::R11D);
+    Reserved.set(X86::R11W);
+    Reserved.set(X86::R11B);
+  }
+
   return Reserved;
 }
 
