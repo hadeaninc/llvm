4a656f6b09267716a7fe47ef37ee6c3b28385e5e
David Brazdil <david@hadean.com>
2017-08-01 15:39:52 +0000

    Add test

--- test/MC/X86/Hadean/syscall.s
+++ test/MC/X86/Hadean/syscall.s
@@ -4,3 +4,11 @@
   mov $60, %rax
   syscall
   hlt
+
+// CHECK:      {{^\.text:}}
+// CHECK-NEXT:   movq    $60, %rax
+// CHECK-NEXT:   movabsq $0, %rcx
+// CHECK-NEXT:   nopl
+// CHECK-NEXT:   nopw    -83(%rsi,%rbx,8)
+// CHECK-NEXT:   syscall
+// CHECK-NEXT:   hlt
